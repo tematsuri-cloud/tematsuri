751d6e52202b5a70beeb87cd340819facc4a5cf9
tematsuri-cloud <tematsuri@gmail.com>
2026-02-11 19:49:40 +0900

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -3,3 +3,14 @@ function isNowActive(title) {
   const now = new Date();
   return now >= new Date(title.start) && now <= new Date(title.end);
 }
+function isOngoing(titleId, date) {
+  const period = titlePeriods[titleId];
+  if (!period) return false;
+
+  const year = date.getFullYear();
+
+  const start = new Date(`${year}-${period.start}`);
+  const end = new Date(`${year}-${period.end}`);
+
+  return date >= start && date <= end;
+}
