783e3a223b74e0582c97fc63b161e649ba7ded62
tematsuri-cloud <tematsuri@gmail.com>
2026-02-11 11:01:06 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,67 @@
 
+const TITLE_MATCHES = [
+  {
+    id: "1oushou",
+    nameJa: "王将戦",
+    nameEn: "Ousho Championship",
+    start: "2026-01-01T21:00:00",
+    end:   "2026-02-25T21:00:00",
+    json: "data/oushou.json"
+  },
+  {
+    id: "2kiou",
+    nameJa: "棋王戦",
+    nameEn: "Kiou Championship",
+    start: "2026-02-25T21:00:00",
+    end:   "2026-04-25T21:00:00",
+    json: "data/kiou.json"
+  },
+  {
+    id: "3meijin",
+    nameJa: "名人戦",
+    nameEn: "Meijin Championship",
+    start: "2026-04-25T21:00:00",
+    end:   "2026-05-25T21:00:00",
+    json: "data/meijin.json"
+  },
+  {
+    id: "4eiou",
+    nameJa: "叡王戦",
+    nameEn: "Eiou Championship",
+    start: "2026-05-25T21:00:00",
+    end:   "2026-06-25T21:00:00",
+    json: "data/eiou.json"
+  },
+  {
+    id: "5kisei",
+    nameJa: "棋聖戦",
+    nameEn: "Kisei Championship",
+    start: "2026-06-25T21:00:00",
+    end:   "2026-08-01T21:00:00",
+    json: "data/kisei.json"
+  },
+  {
+    id: "6oui",
+    nameJa: "王位戦",
+    nameEn: "Oui Championship",
+    start: "2026-08-01T21:00:00",
+    end:   "2026-09-10T21:00:00",
+    json: "data/oui.json"
+  },
+  {
+    id: "7ouza",
+    nameJa: "王座戦",
+    nameEn: "Ouza Championship",
+    start: "2026-09-10T21:00:00",
+    end:   "2026-11-15T21:00:00",
+    json: "data/ouza.json"
+  },
+  {
+    id: "8ryuou",
+    nameJa: "竜王戦",
+    nameEn: "Ryuou Championship",
+    start: "2026-11-15T21:00:00",
+    end:   "2026-12-31T23:59:00",
+    json: "data/ryuou.json"
+  }
+];
