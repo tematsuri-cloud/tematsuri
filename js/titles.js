e7edb57c8e17d450f6daa80796a07344e78ef86c
tematsuri-cloud <tematsuri@gmail.com>
2026-02-22 14:50:51 +0900

    Update titles.js

--- js/titles.js
+++ js/titles.js
@@ -1,58 +1,58 @@
 const titles = [
   {
     id: "oushou",
+    stat: "1oushou",
     name: "王将戦",
     start: "2026-01-01",
-    end: "2026-02-25",
-    updated: "2026-02-11"
+    end: "2026-02-25"
   },
   {
     id: "kiou",
+    stat: "2kiou",
     name: "棋王戦",
     start: "2026-02-25",
-    end: "2026-04-25",
-    updated: "2026-02-11"
+    end: "2026-04-25"
   },
   {
     id: "meijin",
+    stat: "3meijin",
     name: "名人戦",
     start: "2026-04-25",
-    end: "2026-05-25",
-    updated: "2026-02-11"
+    end: "2026-05-25"
   },
   {
     id: "eiou",
+    stat: "4eiou",
     name: "叡王戦",
     start: "2026-05-25",
-    end: "2026-06-25",
-    updated: "2026-02-11"
+    end: "2026-06-25"
   },
   {
     id: "kisei",
+    stat: "5kisei",
     name: "棋聖戦",
     start: "2026-06-25",
-    end: "2026-08-01",
-    updated: "2026-02-11"
+    end: "2026-08-01"
   },
   {
     id: "oui",
+    stat: "6oui",
     name: "王位戦",
     start: "2026-08-01",
-    end: "2026-09-10",
-    updated: "2026-02-11"
+    end: "2026-09-10"
   },
   {
     id: "ouza",
+    stat: "7ouza",
     name: "王座戦",
     start: "2026-09-10",
-    end: "2026-11-15",
-    updated: "2026-02-11"
+    end: "2026-11-15"
   },
   {
     id: "ryuou",
+    stat: "8ryuou",
     name: "竜王戦",
     start: "2026-11-15",
-    end: "2026-12-31",
-    updated: "2026-02-11"
+    end: "2026-12-31"
   }
 ];
