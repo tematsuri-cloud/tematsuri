ca1598bac903cac0078d5545c30a4c84759487cf
tematsuri-cloud <tematsuri@gmail.com>
2026-04-04 17:49:43 +0900

    Update titles.js

--- js/titles.js
+++ js/titles.js
@@ -2,56 +2,56 @@ const titles = [
   {
     id: "oushou",
     stat: "1oushou",
-    name: "王将戦",
+    name: "王将杯",
     start: "2026-01-01",
     end: "2026-02-25"
   },
   {
     id: "kiou",
     stat: "2kiou",
-    name: "棋王戦",
+    name: "棋王杯",
     start: "2026-02-25",
     end: "2026-04-25"
   },
   {
     id: "meijin",
     stat: "3meijin",
-    name: "名人戦",
+    name: "名人杯",
     start: "2026-04-25",
     end: "2026-05-25"
   },
   {
     id: "eiou",
     stat: "4eiou",
-    name: "叡王戦",
+    name: "叡王杯",
     start: "2026-05-25",
     end: "2026-06-25"
   },
   {
     id: "kisei",
     stat: "5kisei",
-    name: "棋聖戦",
+    name: "棋聖杯",
     start: "2026-06-25",
     end: "2026-08-01"
   },
   {
     id: "oui",
     stat: "6oui",
-    name: "王位戦",
+    name: "王位杯",
     start: "2026-08-01",
     end: "2026-09-10"
   },
   {
     id: "ouza",
     stat: "7ouza",
-    name: "王座戦",
+    name: "王座杯",
     start: "2026-09-10",
     end: "2026-11-15"
   },
   {
     id: "ryuou",
     stat: "8ryuou",
-    name: "竜王戦",
+    name: "竜王杯",
     start: "2026-11-15",
     end: "2026-12-31"
   }
